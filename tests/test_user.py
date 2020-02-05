4615a0b626f577d86ecece822da361a61bfe99d9
Wiliam Souza <wiliamsouza83@gmail.com>
2020-02-05 06:06:38 -0300

    Add tests for user use case implementations

---
+++ tests/test_user.py
@@ -0,0 +1,40 @@
+"""Tests for user use case implementations."""
+import datetime
+from unittest.mock import MagicMock, PropertyMock
+from promotion.user import UserUseCase
+
+
+def test_birthday_is_today():
+
+    # This mocks the non exist UserDataStore
+    date = datetime.date.today()
+    user = MagicMock()
+    birthday = PropertyMock(return_value=date)
+    type(user).date_of_birth = birthday
+    store = MagicMock()
+    store.user.return_value = user
+    # TODO: Change to use mock.patch after it exists
+
+    case = UserUseCase(store)
+
+    result = case.birthday(user.id)
+
+    assert result == {"percentage": 5}
+
+
+def test_birthday_is_not_today():
+
+    # This mocks the non exist UserDataStore
+    date = datetime.datetime.strptime("1981-06-06", "%Y-%m-%d").date()
+    user = MagicMock()
+    birthday = PropertyMock(return_value=date)
+    type(user).date_of_birth = birthday
+    store = MagicMock()
+    store.user.return_value = user
+    # TODO: Change to use mock.patch after it exists
+
+    case = UserUseCase(store)
+
+    result = case.birthday(user.id)
+
+    assert result == {"percentage": 0}
