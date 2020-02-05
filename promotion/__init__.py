de3314e49f99f92a0fb8495ca3d58965f0bf4fa6
Wiliam Souza <wiliamsouza83@gmail.com>
2020-02-05 04:28:55 -0300

    Add protocol definitions

---
+++ promotion/__init__.py
@@ -0,0 +1,61 @@
+"Protocol definitions"
+import datetime
+from typing import Dict
+from typing_extensions import Protocol
+
+
+class DiscountDataStore(Protocol):
+    "Data store interface for discounts."
+
+    def __init__(self, database) -> None:
+        ...
+
+    def discount_product_and_user(self, product_id, user_id) -> Dict:
+        "Retrieve discount for the given user and product ID."
+        ...
+
+
+class UserDataStore(Protocol):
+    "Data store interface for users."
+
+    def __init__(self, database) -> None:
+        ...
+
+    def user(self, user_id) -> Dict:
+        "Retrieve user for the given ID."
+        ...
+
+
+class DiscountUseCase(Protocol):
+    "Domain interface for discount bussines logic."
+
+    def __init__(
+        self, store: DiscountDataStore, holiday: "HolidayUseCase", user: "UserUseCase"
+    ) -> None:
+        ...
+
+    def discounts(self, product_id, user_id) -> Dict:
+        """Retrieve all discounts available."""
+        ...
+
+
+class HolidayUseCase(Protocol):
+    """Domain interface for discount by holiday bussines logic."""
+
+    def __init__(self, date: datetime.date) -> None:
+        ...
+
+    def black_friday(self) -> Dict:
+        """Give discount if it is black friday."""
+        ...
+
+
+class UserUseCase(Protocol):
+    "Domain interface for discount by user bussines logic."
+
+    def __init__(self, store: UserDataStore) -> None:
+        ...
+
+    def birthday(self, user_id) -> Dict:
+        """Give discount if is the exact date a person was born."""
+        ...
