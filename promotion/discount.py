e57bd9342bc65624e8c6aecd056e140b52165742
Wiliam Souza <wiliamsouza83@gmail.com>
2020-02-05 06:42:05 -0300

    Add discount use case implementation

---
+++ promotion/discount.py
@@ -0,0 +1,42 @@
+"Discount use case implementation."
+
+
+class DiscountUseCase:
+    """Implements discount use case interface."""
+
+    def __init__(self, store, holiday, user):
+        self.store = store
+        self.holiday = holiday
+        self.user = user
+
+    def discounts(self, product_id, user_id):
+        """Retrieve all discounts available.
+
+        Use cases implemented:
+
+        * Product
+        * User and product
+        * User birthday
+        * Black friday holiday
+        """
+        discount = {"percentage": 0}
+        total_percentage = 0
+
+        by_product_and_user = self.store.discount_product_and_user(product_id, user_id)
+        if by_product_and_user:
+            total_percentage += by_product_and_user.percentage
+
+        by_birthday = self.user.birthday(user_id)
+        if by_birthday:
+            total_percentage += by_birthday["percentage"]
+
+        by_holiday = self.holiday.black_friday()
+        if by_holiday:
+            total_percentage += by_holiday["percentage"]
+
+        discount["percentage"] = total_percentage
+        if total_percentage > 10:
+            # TODO: Change to get from environment variable
+            discount["percentage"] = 10
+
+        return discount
