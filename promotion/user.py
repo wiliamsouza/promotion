5d07ae7a5069175d49eeb7db242b8341294d7b02
Wiliam Souza <wiliamsouza83@gmail.com>
2020-02-05 06:07:53 -0300

    Add user use case implementation

---
+++ promotion/user.py
@@ -0,0 +1,19 @@
+"User use case implementation."
+import datetime
+
+
+class UserUseCase:
+    """Implements user use case interface."""
+
+    def __init__(self, store):
+        self.store = store
+
+    def birthday(self, user_id):
+        """Birthday discount percentage."""
+        discount = {"percentage": 0}
+        user = self.store.user(user_id)
+        if user and user.date_of_birth == datetime.date.today():
+            # TODO: Change to get from environment variable
+            discount["percentage"] = 5
+
+        return discount
