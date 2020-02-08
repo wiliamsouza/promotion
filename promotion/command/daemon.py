7d1314af75f6164c9547259fed6d0c71ace83c86
Wiliam Souza <wiliamsouza83@gmail.com>
2020-02-07 23:22:42 -0300

    Add promotion command line interface to serve gRPC

---
+++ promotion/command/daemon.py
@@ -0,0 +1,58 @@
+"""Daemon CLI"""
+import datetime
+from concurrent import futures
+
+import click
+
+import grpc
+
+from prettyconf import config
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+from promotion.grpc.v1alpha1 import promotion_api_pb2_grpc as service
+from promotion.grpc.server import DiscountServicer
+from promotion.discount import DiscountUseCase
+from promotion.holiday import HolidayUseCase
+from promotion.product import ProductUseCase
+from promotion.user import UserUseCase
+from promotion.postgresql.user import UserDataStore
+from promotion.postgresql.product import ProductDataStore
+
+
+@click.group()
+def cli():
+    """Discount daemon command line interface."""
+
+
+@cli.group()
+def serve():
+    """Start daemon."""
+
+
+@serve.command("grpc")
+def _grpc():
+    """Start discount gRPC server."""
+    engine = create_engine(config("DATABASE_URL"))
+    Session = sessionmaker(bind=engine)
+    connection = engine.connect()
+    session = Session(bind=connection)
+
+    user_store = UserDataStore(session)
+    user_case = UserUseCase(user_store)
+
+    date = datetime.date.today()
+    holiday_case = HolidayUseCase(date)
+
+    product_store = ProductDataStore(session)
+    product_case = ProductUseCase(product_store)
+
+    case = DiscountUseCase(product_case, holiday_case, user_case)
+
+    servicer = DiscountServicer(case)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    service.add_DiscountAPIServicer_to_server(servicer, server)
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
