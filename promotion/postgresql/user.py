adce9e6bfeae6034f35bde3818f40a92ec0700ab
Wiliam Souza <wiliamsouza83@gmail.com>
2020-02-13 13:00:24 -0300

    Change user data store to use entity

--- promotion/postgresql/user.py
+++ promotion/postgresql/user.py
@@ -1,22 +1,24 @@
 """User data store implementation."""
+import datetime
+from typing import Dict
+import uuid
+
 from promotion.postgresql import User
+from promotion.entity import User as UserEntity
 
 
 class UserDataStore:
     """Implements user data store interface."""
 
-    def __init__(self, database):
+    def __init__(self, database) -> None:
         self.database = database
 
-    def query(self, user_id):
-        # TODO: Change to return entity.User
-        return self.user(user_id)
-
-    def user(self, user_id):
+    def query(self, user_id: uuid.UUID) -> UserEntity:
         "Query filtering user for the given ID."
-        return self.database.query(User).filter(User.id == user_id).first()
+        user = self.database.query(User).filter(User.id == user_id).first()
+        return UserEntity(birthday=user.birthday)
 
-    def create(self, user_id, birthday):
+    def create(self, user_id: uuid.UUID, birthday: datetime.date):
         """Store an user in database."""
         user = User(id=user_id, birthday=birthday)
         self.database.add(user)
