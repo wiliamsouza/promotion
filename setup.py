c1897ce5dbe5d9ec5f22e509075107f9cfba2da4
Wiliam Souza <wiliamsouza83@gmail.com>
2020-03-03 22:09:15 -0300

    Add version to grpcio grpcio-tools and changed circleci docker to use buster

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     include_package_data=True,
     install_requires=[
         "Click",
-        "grpcio",
-        "grpcio-tools",
+        "grpcio==1.27.2",
+        "grpcio-tools==1.27.2",
         "typing-extensions",
         "psycopg2",
         "SQLAlchemy",
