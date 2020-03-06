3a1b01f898006d6d4d6f9d385c89dbd40e516580
Wiliam Souza <wiliamsouza83@gmail.com>
2020-03-06 06:43:27 -0300

    Change default value for tracer endpoint config

--- promotion/settings/__init__.py
+++ promotion/settings/__init__.py
@@ -12,5 +12,5 @@ BLACK_FRIDAY_DATE = datetime.datetime.strptime(
 USER_BIRTHDAY_PERCENTAGE = config("USER_BIRTHDAY_PERCENTAGE", cast=decimal.Decimal)
 MAX_DISCOUNT_PERCENTAGE = config("MAX_DISCOUNT_PERCENTAGE", cast=decimal.Decimal)
 BLACK_FRIDAY_PERCENTAGE = config("BLACK_FRIDAY_PERCENTAGE", cast=decimal.Decimal)
-TRACER_ENDPOINT_HOST = config("TRACER_ENDPOINT_HOST", cast=str, default=None)
-TRACER_ENDPOINT_PORT = config("TRACER_ENDPOINT_PORT", cast=int, default=None)
+TRACER_ENDPOINT_HOST = config("TRACER_ENDPOINT_HOST", cast=str, default="")
+TRACER_ENDPOINT_PORT = config("TRACER_ENDPOINT_PORT", cast=int, default=0)
