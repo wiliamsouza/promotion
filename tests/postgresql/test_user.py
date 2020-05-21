cc35b8971c93d7b78a4186d1d56fc11e74df1529
Wiliam Souza <wiliamsouza83@gmail.com>
2020-05-21 14:53:02 -0300

    Change user data store to more robust

--- tests/postgresql/test_user.py
+++ tests/postgresql/test_user.py
@@ -1,5 +1,6 @@
 """Tests for postgresql user data store implementation."""
 import datetime
+import uuid
 
 from promotion.postgresql.user import UserDataStore
 from promotion.postgresql import User
@@ -7,11 +8,18 @@ from promotion.postgresql import User
 from ..factories import UserFactory
 
 
-def test_user_data_store(database, tracer):
-    user = UserFactory.create()
+def test_query_user_data_store(database, tracer):
+    uid = uuid.uuid4()
+    user = UserFactory.create(id=uid)
     assert database.query(User).one()
     store = UserDataStore(database, tracer)
 
     result = store.query(user.id)
 
+    assert result.user_id == user.id
     assert result.birthday == datetime.date.today()
+    assert result.identity == user.identity
+    assert result.email == user.email
+    assert result.name == user.name
+    # TODO: Change it to be a secure password hash
+    assert result.password == user.password
