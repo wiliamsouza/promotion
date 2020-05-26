71056488ba60b300c82f8d7f2914d7beddb9bdaf
Wiliam Souza <wiliamsouza83@gmail.com>
2020-05-26 16:52:17 -0300

    Add list orders with cashback order use case

--- promotion/order.py
+++ promotion/order.py
@@ -36,9 +36,34 @@ class OrderUseCase:
 
         For a configured list of identity the status will be "approved".
         """
-        with self.tracer.start_as_current_span("OrderUseCase.create", kind=SERVER):
+        with self.tracer.start_as_current_span("OrderUseCase.place_order", kind=SERVER):
 
             if identity in settings.APPROVED_ORDER_IDENTITIES:
                 status = "approved"
 
             return self.store.create(code, identity, amount, status, date)
+
+    def calculate_cashback(self, amount_cents):
+        percentage = 10
+
+        if 100000 < amount_cents <= 150000:
+            percentage = 15
+
+        if 150000 < amount_cents:
+            percentage = 20
+
+        amount = (percentage * amount_cents) / 100.0
+        return (int(amount), percentage)
+
+    def list_orders_with_cashback(self):
+        """List orders with cashback."""
+        with self.tracer.start_as_current_span("OrderUseCase.list_orders_with_cashback", kind=SERVER):
+            orders = []
+            entities = self.store.query_all()
+            for entity in entities:
+                amount, percentage = self.calculate_cashback(entity.amount_cents)
+                entity.amount_cashback_cents = amount
+                entity.cashback_percentage = percentage
+                orders.append(entity)
+            return orders
+
--- tests/test_order.py
+++ tests/test_order.py
@@ -5,6 +5,8 @@ from unittest import mock
 
 from promotion.order import OrderUseCase
 
+from .factories import OrderFactory
+
 
 @mock.patch("promotion.postgresql.order.OrderDataStore")
 def test_place_order(store_mock, tracer):
@@ -37,3 +39,66 @@ def test_place_order_approved_status(store_mock, tracer):
     result = case.place_order(code, identity, amount, date, "nonecziste")
 
     store_mock.create.assert_called_once_with(code, identity, amount, 'approved', date)
+
+@mock.patch("promotion.postgresql.order.OrderDataStore")
+def test_cashback_up_to_a_thousand_100(store_mock, tracer):
+    orders = OrderFactory.build_batch(5, amount_cents=10000)
+    store_mock.query_all.return_value = orders
+
+    case = OrderUseCase(store_mock, tracer)
+
+    results = case.list_orders_with_cashback()
+
+    assert len(results) == 5
+    result = results[0]
+    assert result.amount_cashback_cents == 1000
+    assert result.cashback_percentage == 10.0
+    store_mock.query_all.assert_called()
+
+
+@mock.patch("promotion.postgresql.order.OrderDataStore")
+def test_cashback_up_to_a_thousand(store_mock, tracer):
+    orders = OrderFactory.build_batch(5, amount_cents=100000)
+    store_mock.query_all.return_value = orders
+
+    case = OrderUseCase(store_mock, tracer)
+
+    results = case.list_orders_with_cashback()
+
+    assert len(results) == 5
+    result = results[0]
+    assert result.amount_cashback_cents == 10000
+    assert result.cashback_percentage == 10.0
+    store_mock.query_all.assert_called()
+
+
+@mock.patch("promotion.postgresql.order.OrderDataStore")
+def test_cashback_from_thousand_to_thousand_five_hundred(store_mock, tracer):
+    orders = OrderFactory.build_batch(5, amount_cents=150000)
+    store_mock.query_all.return_value = orders
+
+    case = OrderUseCase(store_mock, tracer)
+
+    results = case.list_orders_with_cashback()
+
+    assert len(results) == 5
+    result = results[0]
+    assert result.amount_cashback_cents == 22500
+    assert result.cashback_percentage == 15.0
+    store_mock.query_all.assert_called()
+
+
+@mock.patch("promotion.postgresql.order.OrderDataStore")
+def test_cashback_more_than_five_hundred(store_mock, tracer):
+    orders = OrderFactory.build_batch(5, amount_cents=200000)
+    store_mock.query_all.return_value = orders
+
+    case = OrderUseCase(store_mock, tracer)
+
+    results = case.list_orders_with_cashback()
+
+    assert len(results) == 5
+    result = results[0]
+    assert result.amount_cashback_cents == 40000
+    assert result.cashback_percentage == 20.0
+    store_mock.query_all.assert_called()
